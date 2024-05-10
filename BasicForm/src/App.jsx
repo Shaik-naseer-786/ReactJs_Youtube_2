dfe89b7c66ac64c9960c88d06004ab6a79e958e9
Naseer <sknaseer9347@gmail.com>
2024-05-10 17:47:05 +0530

    Basic Form added

---
+++ BasicForm/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Form from "./components/Form"
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+        <h1>Hello World</h1>
+        <Form></Form>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ BasicForm/src/components/Form.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            email: '',
+            password: ''
+        };
+    }
+
+    handleChange = e => {
+        const { name, value } = e.target;
+        console.log(name)
+        console.log(value)
+        this.setState({
+            [name]: value
+        });
+    };
+
+    handleSubmit = e => {
+        
+        // Handle form submission, e.g., send data to server
+        alert(`Hello ${this.state.name} , Your Email is ${this.state.email}`)
+
+        console.log(this.state);
+        e.preventDefault();
+    };
+
+    render() {
+        const { name, email, password } = this.state;
+
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Name"
+                    name="name"
+                    value={name}
+                    onChange={this.handleChange}
+                />
+                <input
+                    type="email"
+                    placeholder="Email"
+                    name="email"
+                    value={email}
+                    onChange={this.handleChange}
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    value={password}
+                    onChange={this.handleChange}
+                />
+                <button type="submit">Register</button>
+            </form>
+        );
+    }
+}
+
+export default Form;
